830f3d7083cf13ba8d0390b3b2cf8aa5fd286998
Raphael <raphael.weikl.hi@gmail.com>
2024-05-07 20:44:00 +0200

    Stable Version 2.1.9.7 + Loading Screen on startup

---
+++ swiftKEY_V2/Windows/LoadingScreen.xaml.cs
@@ -0,0 +1,41 @@
+﻿using NAudio.CoreAudioApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Shapes;
+
+namespace swiftKEY_V2
+{
+    public partial class LoadingScreen : Window
+    {
+        public LoadingScreen()
+        {
+            InitializeComponent();
+            Loaded += LoadingScreen_Loaded;
+        }
+
+        private async void LoadingScreen_Loaded(object sender, RoutedEventArgs e)
+        {
+            progressBar.Maximum = 125;
+
+            for(int i = 0; i < 125; i++)
+            {
+                progressBar.Value = i;
+                await Task.Delay(1);
+            }
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+    }
+}
